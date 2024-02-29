3066211e9d876dd8b5a4b20b77916edeadd9157e
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-02-29 11:08:10 +0100

    Core.

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,99 @@ import { useState } from "react";
 import "./App.css";
 
 export default function App() {
-
   //TODO: Add your state fields here
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [complaint, setComplaint] = useState("");
+  const [contact, setContact] = useState("");
+  const [consent, setConsent] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userData = {
+      name,
+      address,
+      phone,
+      email,
+      complaint,
+      contact,
+      consent,
+    };
+    console.log("Form submitted: ", { userData });
+  };
+
+  const handleNameInput = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleAddressInput = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const handlePhoneInput = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handleEmailInput = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleComplaintInput = (event) => {
+    setComplaint(event.target.value);
+  };
+
+  const handleContactInput = (event) => {
+    setContact(event.target.value);
+  };
+
+  const handleConsentCheckbox = (event) => {
+    setConsent(event.target.checked);
+  };
 
   return (
     <>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h2>Complaining form!</h2>
         <div className="form__section-left">
           <label>
             Full name
-            <input type="text" name="name" required />
+            <input
+              type="text"
+              name="name"
+              required
+              onChange={handleNameInput}
+              value={name}
+            />
           </label>
           <label>
             Address
-            <input type="text" name="address" />
+            <input
+              type="text"
+              name="address"
+              onChange={handleAddressInput}
+              value={address}
+            />
           </label>
           <label>
             Phone Number
-            <input type="tel" name="phone" />
+            <input
+              type="tel"
+              name="phone"
+              onChange={handlePhoneInput}
+              value={phone}
+            />
           </label>
 
           <label>
             Email
-            <input type="email" name="email" />
+            <input
+              type="email"
+              name="email"
+              onChange={handleEmailInput}
+              value={email}
+            />
           </label>
         </div>
 
@@ -36,35 +105,67 @@ export default function App() {
               name="complaint"
               rows="10"
               placeholder="You can complain here"
+              onChange={handleComplaintInput}
+              value={complaint}
             ></textarea>
           </label>
 
           <div className="form__radio-group">
             <p>How do you want to be contacted? </p>
             <label>
-              <input type="radio" name="contact" value="phone" />
+              <input
+                type="radio"
+                name="contact"
+                value="phone"
+                onChange={handleContactInput}
+                checked={contact === "phone"}
+              />
               Phone
             </label>
 
             <label>
-              <input type="radio" name="contact" value="email" />
+              <input
+                type="radio"
+                name="contact"
+                value="email"
+                onChange={handleContactInput}
+                checked={contact === "email"}
+              />
               Email
             </label>
 
             <label>
-              <input type="radio" name="contact" value="post" />
+              <input
+                type="radio"
+                name="contact"
+                value="post"
+                onChange={handleContactInput}
+                checked={contact === "post"}
+              />
               Slow Mail
             </label>
 
             <label>
-              <input type="radio" name="contact" value="none" />
+              <input
+                type="radio"
+                name="contact"
+                value="none"
+                onChange={handleContactInput}
+                checked={contact === "none"}
+              />
               No contact!
             </label>
           </div>
 
           <label>
             I agree you take my data, and do whatever
-            <input type="checkbox" name="consent" id="consent" />
+            <input
+              type="checkbox"
+              name="consent"
+              id="consent"
+              onChange={handleConsentCheckbox}
+              checked={consent}
+            />
           </label>
         </div>
         <input type="submit" value="Submit!" />
